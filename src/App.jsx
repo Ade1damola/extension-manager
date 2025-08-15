66bf77771efa8bfe41a967ac9ee3847b704d4644
Ade1damola <adedamola.adenuga04@gmail.com>
2025-08-15 12:12:24 +0100

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <div className="bg-gradient-to-br from-[#0B1220] to-[#1C2942] text-white min-h-screen px-4 sm:px-32 py-8 font-sans">
-        <Navbar />
+        {/* <Navbar /> */}
         <Body />
       </div>
     </>
